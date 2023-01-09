c88b9ea8384e2ddf5fc637cb0c66ea8a1ad0a782
olya alyakina <olgast97@gmail.com>
2023-01-09 09:23:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountTicket = 5000;
+        int mile = 20;
+        int accruedMile = amountTicket/mile;
+
+        System.out.println("Начислено милей: " + accruedMile);
+    }
+}
